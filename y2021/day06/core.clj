910e587a40b85c738404be5ed9430512a7333f05
vollcheck <j3k.walczak@gmail.com>
2021-12-07 15:53:11 +0100

    day 07

--- y2021/day06/core.clj
+++ y2021/day06/core.clj
@@ -17,6 +17,11 @@
        (conj c (dec f))))
    [] fishes))
 
+(defn run-day2 [fishes]
+  (frequencies fishes))
+
+;; (run-day2 small-fishes)
+
 (defn run-days [start-fishes
                 ^Integer days]
   (loop [fs start-fishes
@@ -26,8 +31,15 @@
       (recur (run-day fs) (inc cnt)))))
 
 (def fishes (load-input "input.txt"))
+(def small-fishes (load-input "sample.txt"))
 
 (comment
   (println (str "First part: " (run-days fishes 80)))
   (println (str "Second part: " (run-days fishes 256)))
   )
+
+;; So rather than maintaining a list of each individual fish you can use 'frequencies' on the initial list, then each step is just updating the frequencies (and adding new fish when appropriate) .
+
+;; My results return immediately that way.
+
+;; I probably need to clean my code up a bit since I wrote it in the middle of the night but that method will be much more efficient.
---
+++ y2021/day07/core.clj
@@ -0,0 +1,50 @@
+(ns day07
+  (:require [clojure.string :as str]))
+
+(defn load-input [filename]
+  ;; May I should use transducer one day.
+  (->> filename
+       (slurp)
+       (str/trim)
+       ((fn [data] (str/split data #",")))
+       (map #(Integer/parseUnsignedInt %))
+       (into [])))
+
+(defn mean [coll]
+  (let [sum (apply + coll)
+        count (count coll)]
+    (if (pos? count)
+      (/ sum count)
+      0)))
+
+(defn median [coll]
+  (let [sorted (sort coll)
+        cnt (count sorted)
+        halfway (quot cnt 2)]
+    (if (odd? cnt)
+      (nth sorted halfway)
+      (let [bottom (dec halfway)
+            bottom-val (nth sorted bottom)
+            top-val (nth sorted halfway)]
+        (mean [bottom-val top-val])))))
+
+(defn count-fuel [coll]
+  (let [m (median coll)]
+    (reduce
+     (fn [counter number]
+       (+ counter (Math/abs (- number m))))
+     0 coll)))
+
+(defn move [end]
+  (/ (* end (+ end 1)) 2))
+
+(defn fuel-cost [crab position]
+  (move (Math/abs (- crab position))))
+
+(defn count-fuel2 [coll]
+  (apply min
+         (for [position (range 1 (inc (apply max coll)))]
+           (reduce + (map #(fuel-cost % position) coll)))))
+
+(println (count-fuel (load-input "input.txt")))
+(println (count-fuel2 (load-input "input.txt")))
