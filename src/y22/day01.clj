4644e17fcc79e2cf2964bae663c79b82049cd831
vollcheck <jaw@juxt.pro>
2022-12-01 08:09:23 +0100

    y22: let the game begin!

--- src/y22/day01.clj
+++ src/y22/day01.clj
@@ -1,4 +1,33 @@
 (ns y22.day01
-  (:require [user]))
+  (:require [clojure.string :as str]))
 
-(user/parse-int "3")
+(defn load-input []
+  (slurp "src/y22/input01.txt"))
+
+(defn parse-group [group]
+  (->> group
+       str/split-lines
+       (map #(Integer/parseInt %))
+       (apply +)
+       ))
+
+(defn part-1 [input]
+  (->> (str/split input #"\n\n")
+       (map parse-group)
+       (apply max)
+       ))
+
+(defn part-2 [input]
+  (->> (str/split input #"\n\n")
+       (map parse-group)
+       (sort >)
+       (take 3)
+       (apply +)
+       ))
+
+
+(comment
+  (def input (load-input))
+  (part-1 input) ;; => 72017
+  (part-2 input) ;; => 212520
+  )
