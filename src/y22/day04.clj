1878a3a5e3a5e37f57ff883a24c95cf281d8457a
vollcheck <jaw@juxt.pro>
2023-04-13 18:26:34 +0200

    y22.d04: make it faster
    from:
    
    Evaluation count : 528 in 6 samples of 88 calls.
                 Execution time mean : 1.270966 ms
        Execution time std-deviation : 116.634343 µs
       Execution time lower quantile : 1.169070 ms ( 2.5%)
       Execution time upper quantile : 1.415971 ms (97.5%)
                       Overhead used : 5.778081 ns
    
    to:
    
    Evaluation count : 1554 in 6 samples of 259 calls.
                 Execution time mean : 437.473643 µs
        Execution time std-deviation : 82.047079 µs
       Execution time lower quantile : 391.219819 µs ( 2.5%)
       Execution time upper quantile : 578.692050 µs (97.5%)
                       Overhead used : 5.778081 ns
    
    Found 1 outliers in 6 samples (16.6667 %)
    	low-severe	 1 (16.6667 %)
     Variance from outliers : 48.1491 % Variance is moderately inflated by
     outliers
    
    that might be the material for a blog post

--- src/y22/day04.clj
+++ src/y22/day04.clj
@@ -1,46 +1,44 @@
 (ns y22.day04
   (:require [clojure.string :as str]
-            [core :refer [load-input]]))
+            [core :refer [load-in]]))
 
-(defn sub? [line]
-  (let [[f s] (str/split line #",")
-        [fstart fend] (str/split f #"-")
-        [sstart send] (str/split s #"-")
-        [fstart fend sstart send] (map parse-long [fstart fend sstart send])]
+(defn- parse-line [^String line]
+  (mapv parse-long (str/split line #"[,-]")))
+
+(defn sub? [^String line]
+  (let [[fstart fend sstart send] (parse-line line)]
     (or
      (and (<= sstart fstart)
           (>= send fend))
      (and (>= sstart fstart)
           (<= send fend)))))
 
-(defn overlap? [line]
-  (let [[f s] (str/split line #",")
-        [fstart fend] (str/split f #"-")
-        [sstart send] (str/split s #"-")
-        [fstart fend sstart send] (map parse-long [fstart fend sstart send])]
+(defn overlap? [^String line]
+  (let [[fstart fend sstart send] (parse-line line)]
     (or (<= sstart fstart send)
-        (<= sstart fend send)
-        (<= fstart sstart fend)
-        (<= fstart send fend))))
+        (<= fstart sstart fend))))
 
-
-(defn part-1 [input]
-  (->> input
-       (filter sub?)
-       count
-       ))
-
-(defn part-2 [input]
-  (->> input
-       (filter overlap?)
-       count
-       ))
+(defn solve [input ff]
+  (count (filter ff input)))
 
 (comment
-  (def test-input (load-input :test))
-  (def input (load-input))
-  (part-1 test-input) ;; => 2
-  (part-1 input) ;; => 571
-  (part-2 test-input) ;; => 4
-  (part-2 input) ;; => 917
+  (def test-input (load-in :test))
+  (def input (load-in))
+
+  ;; first part
+  (solve input sub?)
+   ;; => 571
+
+  ;; second part
+  (solve input overlap?)
+   ;; => 917
+
+  (require '[criterium.core :as criterium])
+  (criterium/quick-bench (solve input sub?))
+  (criterium/quick-bench (solve input overlap?))
+
+  (require '[clj-async-profiler.core :as prof])
+  (prof/profile (solve input sub?))
+  (prof/profile (solve input overlap?))
+  (prof/serve-ui 54321)
   )
