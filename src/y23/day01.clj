705750619cbea42c0ed473c296499106bb4a61c1
vollcheck <jaw@juxt.pro>
2023-12-01 10:52:24 +0100

    y23d01

---
+++ src/y23/day01.clj
@@ -0,0 +1,49 @@
+(ns y23.day01
+  (:require [core :refer [load-in]]
+            [clojure.string :as str]))
+
+(defn parse-line-1 [line]
+  (->> (re-seq #"[0-9]" line)
+       ((juxt first last))
+       (str/join "")
+       parse-long))
+
+(defn part-1 [lines]
+  (->> lines
+       (map parse-line-1)
+       (reduce + 0)))
+
+(comment
+  (part-1 (load-in))
+  )
+
+(def literal
+  ["one"
+   "two"
+   "three"
+   "four"
+   "five"
+   "six"
+   "seven"
+   "eight"
+   "nine"])
+
+(def stoi
+  (zipmap literal (range 1 10)))
+
+(defn parse-line [line]
+  (->>
+   (re-seq #"(?=(one|two|three|four|five|six|seven|eight|nine|[0-9]))" line)
+   (map #(second %))
+   ((juxt first last))
+   (map #(get stoi % %))
+   (str/join "")
+   parse-long))
+
+(defn part-2 [lines]
+  (->> (map parse-line lines)
+       (reduce + 0)))
+
+(comment
+  (part-2 (load-in))
+  )
