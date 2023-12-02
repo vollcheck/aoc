503fa15d5e0f44084316188f9e608451ac0f6af4
vollcheck <jaw@juxt.pro>
2023-12-02 23:51:35 +0100

    add bb commands for new days
    taken from https://github.com/elken/aoc/blob/master/bb.edn

---
+++ resources/template.clj
@@ -0,0 +1,18 @@
+(ns yYEAR.dayDAY
+  (:require [clojure.string :as str]
+            [core :refer [load-in]]))
+
+(defn part-1 [in]
+  )
+
+(defn part-2 [in]
+  )
+
+(comment
+  (def test-input (load-in :test))
+  (def input (load-in))
+  (part-1 test-input)
+  (part-1 input)
+  (part-2 test-input)
+  (part-2 input)
+  )
