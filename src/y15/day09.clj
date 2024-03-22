06d79facff8c8834eee12ddb6a1185a3feea5f39
vollcheck <jaw@juxt.pro>
2024-03-22 17:17:51 +0100

    y15d09: upload

--- src/y15/day09.clj
+++ src/y15/day09.clj
@@ -1,37 +1,53 @@
 (ns y15.day09
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]
+            [core :refer [load-in]]))
 
-;; TODO: UNFINISHED
+(defn parse-line [state line]
+  (let [[from _ to _ dist] (str/split line #" ")]
+    ;; trick: by making key the set, I can do a bidirectional search
+    (assoc state #{from to} (parse-long dist))))
 
-(def dists
-"London to Dublin = 464
-London to Belfast = 518
-Dublin to Belfast = 141")
+(defn get-graph [in]
+  (reduce parse-line {} in))
 
-(defn load-input []
-  )
-
-(defn parse-line [graph line]
-  (let [line (str/split line #" ")
-        [from _ to _ dist] line
-        from (keyword from)
-        to (keyword to)
-        dist (Integer/parseUnsignedInt dist)
-        existing (or (get graph from) {})]
-    (assoc graph from (assoc existing to dist))))
-
-(defn make-dists-graph
-  [lines]
-  (reduce parse-line {} lines))
-
-
+(defn get-cities [in]
+  (reduce
+   (fn [state line]
+     (let [[c1 _ c2] (str/split line #" ")]
+       (into state #{c1 c2})))
+   #{}
+   in))
 
 (comment
-  (def graph (make-dists-graph (str/split-lines dists)))
+  (vec (get-cities (load-in :test)))
+  ;; => ["Belfast" "London" "Dublin"]
+  )
 
-  (parse-line {} "London to Dublin = 464")
-  (def lines (->> dists
-                  (str/split-lines)
-                  (map #(str/split % #" "))))
-  lines
+(defn calculate-trip-dist [graph trip]
+  (->> (partition 2 1 trip)
+       (reduce
+        (fn [dist part-trip]
+          (+ dist (get graph (set part-trip))))
+        0)))
+
+(defn calculate-all-trip-distances [in]
+  (let [graph (get-graph in)
+        cities (get-cities in)]
+    (->> (vec cities)
+         combo/permutations
+         (map (partial calculate-trip-dist graph)))))
+
+(defn solve [f in]
+  (->> (calculate-all-trip-distances in)
+       (apply f)))
+
+(comment
+  (def test-in (load-in :test))
+  (def part-1-test (solve min test-in))
+  (def part-2-test (solve max test-in))
+
+  (def in (load-in))
+  (def part-1 (solve min in))
+  (def part-2 (solve max in))
   )
