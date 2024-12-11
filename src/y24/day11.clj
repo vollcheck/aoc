6bac48bb48974114f7173a1936a9fa2e3a11f4c4
vollcheck <j3k.walczak@gmail.com>
2024-12-11 15:42:51 +0100

    y24d11: optimized a bit

--- src/y24/day11.clj
+++ src/y24/day11.clj
@@ -1,38 +1,38 @@
 (ns y24.day11
-  (:require [clojure.string :as str]
-            [criterium.core :refer [quick-bench]]
-            [core :refer [parse-numbers load-in]]))
+  (:require [core :refer [parse-numbers load-in]]))
 
 (defn update-stones [stones-map]
-  (->> stones-map
-       (map
-        (fn [[k v]]
-          (if (= k 0)
-            {1 v}
+  (reduce
+   (fn [acc [k v]]
+     (let [f (fnil (partial + v) 0)]
+       (if (= k 0)
+         (update acc 1 f)
 
-            (let [s (str k)
-                  len (.length ^String s)
-                  half (/ len 2)]
-              (if (even? len)
-                (let [power (Math/pow 10 half)
-                      fi (int (quot k power))
-                      se (int (rem k power))]
-                  (merge-with + {fi v} {se v}))
+         (let [s (str k)
+               len (.length ^String s)
+               half (/ len 2)]
+           (if (even? len)
+             (let [power (Math/pow 10 half)
+                   fi (int (quot k power))
+                   se (int (rem k power))]
+               (-> (update acc fi f)
+                   (update fi f)))
 
-                  {(* 2024 k) v})))))
-       (apply merge-with +)))
+             (update acc (* 2024 k) f))))))
+   {}
+   stones-map))
 
 (defn solve [times in]
   (->> (parse-numbers in)
        frequencies
        (iterate update-stones)
        (take (inc times))
-       (last)
-       (transduce (map second) +' 0)))
+       last
+       (transduce (map second) + 0)))
 
 (comment
-  (def intest "125 17")
-  (def in "9759 0 256219 60 1175776 113 6 92833")
+  (def intest (load-in :test :no-split))
+  (def in (load-in :no-split))
 
   (solve 25 intest)
   ;; => 55312
@@ -41,4 +41,16 @@
 
   (solve 75 in)
   ;; => 221683913164898
+  )
+
+
+(comment
+  (require '[criterium.core :refer [quick-bench]])
+  (quick-bench (solve 75 in))
+   ;;           Execution time mean : 448.867523 µs
+   ;;  Execution time std-deviation : 29.129477 µs
+   ;; Execution time lower quantile : 432.083285 µs ( 2.5%)
+   ;; Execution time upper quantile : 498.456758 µs (97.5%)
+   ;;                 Overhead used : 9.478232 ns
+
   )
